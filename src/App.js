072c5f1d2930018023db2607ddd55422dae7ba5a
Jonathan Fisher <jfisher94002@gmail.com>
2022-12-29 14:58:46 -0800

    Upgraded front end react allocation page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,46 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // Add code to import the components
+import Budget from './components/Budget';
+import ExpenseList from './components/ExpenseList';
+import ExpenseTotal from './components/ExpenseTotal';
+import Remaining from './components/Remaining';
+import AllocationForm from './components/AllocationForm';
+import Currency from './components/Currency';
+
 import { AppProvider } from './context/AppContext';
 const App = () => {
     return (
         <AppProvider>
             <div className='container'>
-                {/* Add the code here to add the components          */}
-            </div>
+                 <h1 className='mt-3'>Jonathan's Budget Allocation</h1>
+                 <div className='row mt-3'>
+                     <div className='col-sm'>
+                         <Budget />
+                     </div>
+                     <div className='col-sm'>
+                         <Remaining />
+                     </div>
+                     <div className='col-sm'>
+                         <ExpenseTotal />
+                     </div>
+                   <div className='col-sm'>
+                        <Currency />
+                    </div>
+                 </div>
+                 <h3 className='mt-3'>Allocation</h3>
+                 <div className='row '>
+                     <div className='col-sm'>
+                         <ExpenseList />
+                     </div>
+                 </div>
+                 <h3 className='mt-3'>Change allocation</h3>
+                 <div className='row mt-3'>
+                     <div className='col-sm'>
+                         <AllocationForm/>
+                     </div>
+                 </div>
+             </div>
         </AppProvider>
     );
 };
--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,40 @@
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
+
+const Budget = () => {
+	const { budget,dispatch,expenses, currency } = useContext(AppContext);
+
+	const totalExpenses = expenses.reduce((total, item) => {
+    	return (total += item.cost);
+    }, 0);
+
+	const changeBudget = (val) => {
+
+		if (val < totalExpenses) {
+			alert("You cannot reduce the budget that is already allocated!");
+		} else {
+			dispatch({
+				type: 'SET_BUDGET',
+				payload: val,
+			})
+		}
+		console.log(val);
+	}
+
+	return (
+		<div className='alert alert-secondary'>
+	    <span>Budget: {currency}
+			<input type="number"
+			        step="10"
+			        value={budget}
+			        min="0"
+			        max="20000"
+			        id='budget'
+			        onChange={(event) => changeBudget(event.target.value)}>
+			</input>
+		</span>
+		</div>
+	);
+};
+export default Budget;
 
